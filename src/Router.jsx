e9e3e437bbb8a976870939bff197eee1783317b3
Ruyter Araujo Rocha <ruyteraraujo992@gmail.com>
2023-08-21 13:31:44 -0300

    adding components

---
+++ src/Router.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Route, BrowserRouter, Routes } from "react-router-dom";
+import Home from "./components/Home/Home";
+
+//  AQUI TU PODE CRIAR ROTAS E CHAMAR NOVOS ELEMENTOS.
+// PARA CRIAR UM ELEMENTO NOVO, SEGUE ESSE PADRÃO:
+
+/* 
+
+import React from "react";
+import "./caminho/do/css";
+
+function nomeDoComponente() {
+  return <div>Conteudo HTML aqui</div>;
+}
+
+export default nomeDoComponente;
+
+
+*/
+
+// PRA ADICIONAR UMA NOVA ROTA, CRIE UM Route E ADICIONE UM PATH PASSANDO A ROTA E UM element PASSANDO O COMPONENTE CORRESPONDENTE
+
+function Router() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={Home} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default Router;
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./home.css";
+
+function Home() {
+  return (
+    <main>
+      <div className="titles">
+        <h1>So um exemplo</h1>
+      </div>
+    </main>
+  );
+}
+
+export default Home;
