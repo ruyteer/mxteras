ad32e13c27f5736c6166fa593ab111cf325557e5
Vitor Hugo <vitorhugo.snp@gmail.com>
2023-08-22 10:25:29 -0400

    little changes in header

--- src/components/Home/Header.jsx
+++ src/components/Home/Header.jsx
@@ -6,17 +6,33 @@ function Header() {
   return (
     <>
       <header>
-        <h1 className='logo'>Teste</h1>
-        <nav>
-          <ul>
-            <li>
-              <Link to='/'>Home</Link>
-            </li>
-            <li>
-              <Link to='/Checkout'>Checkout</Link>
-            </li>
-          </ul>
-        </nav>
+        <section className='uptop'>
+          <h1>Lorem, ipsum dolor sit amet consectetur adipisicing.</h1>
+        </section>
+        <section className='downtop'>
+          <section className='logo'>
+            <h1 className='logo'>Teste</h1>
+          </section>
+          <section className='navigation'>
+            <section className='searchBar'>
+              <input type='text' placeholder='Search...' />
+            </section>
+            <nav>
+              <ul>
+                <li>
+                  <Link to='/'>Home</Link>
+                </li>
+                <li>
+                  <Link to='/Checkout'>Checkout</Link>
+                </li>
+              </ul>
+            </nav>
+          </section>
+          <section className='others'>
+            <h1>other1</h1>
+            <h1>other2</h1>
+          </section>
+        </section>
       </header>
     </>
   );
