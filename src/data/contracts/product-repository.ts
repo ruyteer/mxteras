2c8cc54b22df56c802f224775f8e9f6e46917c3f
Ruyter Araujo Rocha <ruyteraraujo992@gmail.com>
2023-09-03 18:22:28 -0300

    feat: ensure ProductRepository adds update and find-one

--- src/data/contracts/product-repository.ts
+++ src/data/contracts/product-repository.ts
@@ -3,4 +3,6 @@ import { ProductModel } from "../models/product-model";
 export interface IProductRepository {
   create(product: ProductModel): Promise<void>;
   getAll(): Promise<ProductModel[]>;
+  getOne(id: string): Promise<ProductModel>;
+  update(product: ProductModel): Promise<void>;
 }
