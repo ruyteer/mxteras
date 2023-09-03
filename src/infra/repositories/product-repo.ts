44289c842e36f582201ffc6bb9058e6263f7435d
Ruyter Araujo Rocha <ruyteraraujo992@gmail.com>
2023-09-03 18:45:10 -0300

    feat: add product-repo on infra layer

---
+++ src/infra/repositories/product-repo.ts
@@ -0,0 +1,21 @@
+import { prisma } from "../../config/Prisma";
+import { IProductRepository } from "../../data/contracts/product-repository";
+import { ProductModel } from "../../data/models/product-model";
+
+export class ProductRepository implements IProductRepository {
+  async create(product: ProductModel): Promise<void> {
+    await prisma.product.create({ data: product });
+  }
+
+  async getAll(): Promise<ProductModel[]> {
+    return await prisma.product.findMany();
+  }
+
+  async getOne(id: string): Promise<ProductModel> {
+    return await prisma.product.findUnique({ where: { id } });
+  }
+
+  async update(product: ProductModel, id: string): Promise<void> {
+    await prisma.product.update({ where: { id }, data: product });
+  }
+}
