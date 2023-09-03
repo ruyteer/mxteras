e1dbc6149d0691383a8dccd31ca10d69834dd27a
Ruyter Araujo Rocha <ruyteraraujo992@gmail.com>
2023-09-03 13:49:26 -0300

    fix: change product entity to product model

--- src/data/services/get-all-products.ts
+++ src/data/services/get-all-products.ts
@@ -1,12 +1,12 @@
-import { Product } from "../../domain/entities/product";
 import { GetAllProductUseCase } from "../../domain/usecases/product/get-all-product";
 import { IProductRepository } from "../contracts/product-repository";
 import { MissingProductError } from "../errors/missing-product";
+import { ProductModel } from "../models/product-model";
 
 export class GetAllProductService implements GetAllProductUseCase {
   constructor(private readonly productRepository: IProductRepository) {}
 
-  async getAll(): Promise<Product[]> {
+  async getAll(): Promise<ProductModel[]> {
     const products = await this.productRepository.getAll();
 
     if (products.length === 0) {
