4c9a115f5b9ebce614b0b50e764bb760108e1285
Ruyter Araujo Rocha <ruyteraraujo992@gmail.com>
2023-09-04 15:13:21 -0300

    feat: add product routes file to manage routes of products

---
+++ src/modules/main/app/routes/product-routes.ts
@@ -0,0 +1,19 @@
+import { Router } from "express";
+import { productAdapt } from "../adapters/product";
+import { middlewareAdapt } from "../adapters/middlewares";
+import {
+  makeMiddlewareProduct,
+  makeCreateProduct,
+  makeGetAllProduct,
+} from "../factories";
+
+const productRouter = Router();
+
+productRouter.get("/products", productAdapt(makeGetAllProduct()));
+productRouter.post(
+  "/product/create",
+
+  productAdapt(makeCreateProduct())
+);
+
+export { productRouter };
