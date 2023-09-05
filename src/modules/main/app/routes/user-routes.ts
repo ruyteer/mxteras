4bac30c760a3b5550b7294356aaa73913cf28b7b
Ruyter Araujo Rocha <ruyteraraujo992@gmail.com>
2023-09-05 19:35:51 -0300

    feat: add create and get routes

--- src/modules/main/app/routes/user-routes.ts
+++ src/modules/main/app/routes/user-routes.ts
@@ -1,9 +1,15 @@
 import { Router } from "express";
-import { makeAdminLogin } from "../factories";
+import {
+  makeAdminLogin,
+  makeCreateUserController,
+  makeGetUserController,
+} from "../factories";
 import { productAdapt } from "../adapters/product";
 
 const userRouter = Router();
 
 userRouter.post("/admin/login", productAdapt(makeAdminLogin()));
+userRouter.post("/user/create", productAdapt(makeCreateUserController()));
+userRouter.get("/users", productAdapt(makeGetUserController()));
 
 export { userRouter };
