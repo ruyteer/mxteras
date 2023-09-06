a694c5f3855746b76694a6865a0d7735fa547b59
Ruyter Araujo Rocha <ruyteraraujo992@gmail.com>
2023-09-06 13:25:58 -0300

    fix: change price field to float and add category field

--- prisma/migrations/20230905221505_add_user_schema/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "User" (
-    "id" TEXT NOT NULL,
-    "nickname" TEXT NOT NULL,
-    "number" INTEGER NOT NULL,
-    "email" TEXT NOT NULL,
-
-    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
-);
--- prisma/migrations/20230906153742_resolve_migrations/migration.sql
+++ prisma/migrations/20230906153742_resolve_migrations/migration.sql
@@ -3,9 +3,20 @@ CREATE TABLE "Product" (
     "id" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "description" TEXT NOT NULL,
+    "category" TEXT NOT NULL,
     "price" INTEGER NOT NULL,
     "quantity" INTEGER NOT NULL,
     "images" TEXT[],
 
     CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "nickname" TEXT NOT NULL,
+    "number" INTEGER NOT NULL,
+    "email" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
