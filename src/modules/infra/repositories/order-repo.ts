de14fc91df50308e1faed3b6ce6b01bb9a2d2c46
Ruyter Araujo Rocha <ruyteraraujo992@gmail.com>
2023-09-11 13:28:44 -0300

    feat: add order repository on infra layer

---
+++ src/modules/infra/repositories/order-repo.ts
@@ -0,0 +1,17 @@
+import { prisma } from "../../../config/Prisma";
+import { IOrderRepository } from "../../data/contracts/order-repository";
+import { OrderModel } from "../../data/models/order-model";
+
+export class OrderRepository implements IOrderRepository {
+  async findMany(): Promise<OrderModel[]> {
+    return await prisma.order.findMany();
+  }
+
+  async findUnique(userId: string): Promise<OrderModel> {
+    return await prisma.order.findFirstOrThrow({ where: { userId: userId } });
+  }
+
+  async save(order: OrderModel): Promise<void> {
+    await prisma.order.create({ data: order });
+  }
+}
