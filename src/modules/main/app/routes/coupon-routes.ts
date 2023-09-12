356244212f7d28c8c38e4e71d6e88e7134e69c1c
Ruyter Araujo Rocha <ruyteraraujo992@gmail.com>
2023-09-12 10:43:06 -0300

    fix: add admin middleware to protect create-coupon route

--- src/modules/main/app/routes/coupon-routes.ts
+++ src/modules/main/app/routes/coupon-routes.ts
@@ -2,10 +2,17 @@ import { Router } from "express";
 import { productAdapt } from "../adapters/product";
 import { makeCreateCoupon } from "../factories/coupon/create-coupon";
 import { makeGetCoupon } from "../factories/coupon/get-coupon";
+import { middlewareAdapt } from "../adapters/middlewares";
+import { makeMiddlewareProduct } from "../factories";
 
 const couponRoutes = Router();
 
-couponRoutes.post("/coupon/create", productAdapt(makeCreateCoupon()));
+couponRoutes.post(
+  "/coupon/create",
+  middlewareAdapt(makeMiddlewareProduct()),
+  productAdapt(makeCreateCoupon())
+);
+
 couponRoutes.get("/coupons", productAdapt(makeGetCoupon()));
 
 export { couponRoutes };
