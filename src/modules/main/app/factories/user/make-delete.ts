459ad42bdbb8361023b023ae900bf52cf9a84bc2
Ruyter Araujo Rocha <ruyteraraujo992@gmail.com>
2023-09-12 13:22:26 -0300

    feat: add user factory

---
+++ src/modules/main/app/factories/user/make-delete.ts
@@ -0,0 +1,10 @@
+import { DeleteUserService } from "../../../../data/services/user/delete-user";
+import { UserRepository } from "../../../../infra/repositories/user-repo";
+import { DeleteUserController } from "../../../../presentation/controllers/user/delete-user";
+import { Controller } from "../../../../presentation/protocols";
+
+export const makeGetUserController = (): Controller => {
+  const repository = new UserRepository();
+  const usecase = new DeleteUserService(repository);
+  return new DeleteUserController(usecase);
+};
