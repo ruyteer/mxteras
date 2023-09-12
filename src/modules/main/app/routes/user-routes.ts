ee1f8498dbeaa5d8911b458a23d2796b4be9d6e9
Ruyter Araujo Rocha <ruyteraraujo992@gmail.com>
2023-09-12 13:23:26 -0300

    feat: create delete user route

--- src/modules/main/app/routes/user-routes.ts
+++ src/modules/main/app/routes/user-routes.ts
@@ -2,14 +2,22 @@ import { Router } from "express";
 import {
   makeAdminLogin,
   makeCreateUserController,
+  makeDeleteProduct,
   makeGetUserController,
+  makeMiddlewareProduct,
 } from "../factories";
 import { productAdapt } from "../adapters/product";
+import { middlewareAdapt } from "../adapters/middlewares";
 
 const userRouter = Router();
 
 userRouter.post("/admin/login", productAdapt(makeAdminLogin()));
 userRouter.post("/user/create", productAdapt(makeCreateUserController()));
 userRouter.get("/users", productAdapt(makeGetUserController()));
+userRouter.delete(
+  "/user/delete/:id",
+  middlewareAdapt(makeMiddlewareProduct()),
+  productAdapt(makeDeleteProduct())
+);
 
 export { userRouter };
