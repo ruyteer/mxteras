660417a6b1febf7e9a32fca4448ce473c8dfe83d
Ruyter Araujo Rocha <ruyteraraujo992@gmail.com>
2023-09-13 13:38:22 -0300

    feat: add new infos to mercado pago pix sdk

--- src/modules/infra/services/mercado-pago/pix-mercado-pago.ts
+++ src/modules/infra/services/mercado-pago/pix-mercado-pago.ts
@@ -12,11 +12,19 @@ export class CreatePixMercadoPago implements PixMercadoPago {
     mercadopago.configurations.setAccessToken(process.env.MERCADO_PAGO_ACESS);
 
     try {
+      const orderId = Math.floor(Math.random() * Math.pow(10, 6));
+
       const mpResponse = await mercadopago.payment.create({
         payer: data.formData.payer,
         payment_method_id: data.formData.payment_method_id,
         transaction_amount: data.formData.transaction_amount,
         installments: 1,
+        notification_url: "http://localhost:3000/order/pix/update_payment",
+        callback_url: `http://localhost:5173/payment/success/${orderId}`, // passa um numero aleatorio e la no fornt manda uma req pra atualizar pora esse numero
+        order: {
+          id: orderId,
+          type: "mercadopago",
+        },
       });
 
       const {
