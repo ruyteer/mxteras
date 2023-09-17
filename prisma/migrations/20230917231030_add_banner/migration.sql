fe1bb9465c47d8ce6d7b03248b61345493140802
Ruyter Araujo Rocha <ruyteraraujo992@gmail.com>
2023-09-17 20:21:19 -0300

    feat: create banner entity to modify banners later

---
+++ prisma/migrations/20230917231030_add_banner/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "Banner" (
+    "id" TEXT NOT NULL,
+    "image" TEXT NOT NULL,
+
+    CONSTRAINT "Banner_pkey" PRIMARY KEY ("id")
+);
---
+++ src/domain/entities/banner.ts
@@ -0,0 +1,3 @@
+export type Banner = {
+  image: string;
+};
