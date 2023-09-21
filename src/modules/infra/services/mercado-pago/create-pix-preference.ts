91a7791649c8add45e672bbfad9bf4b8acc108ba
Ruyter Araujo Rocha <ruyteraraujo992@gmail.com>
2023-09-21 16:18:14 -0300

    fix: prefernece change

--- src/modules/infra/services/mercado-pago/create-pix-preference.ts
+++ src/modules/infra/services/mercado-pago/create-pix-preference.ts
@@ -7,49 +7,49 @@ export class CreatePixPreference implements ICreatePreference {
     const orderId = Math.floor(Math.random() * Math.pow(10, 6));
     mercadopago.configurations.setAccessToken(process.env.MERCADO_PAGO_ACESS);
     try {
-      // const mpResponse = await mercadopago.preferences.create({
-      //   items: [
-      //     {
-      //       title: product.name,
-      //       unit_price: product.price,
-      //       quantity: product.quantity,
-      //       picture_url: product.image,
-      //     },
-      //   ],
-      //   binary_mode: true,
-      //   back_urls: {
-      //     success: `https://www.mxteras.com/payment/success/${orderId}`,
-      //   },
-      //   auto_return: "all",
-      //   payment_methods: {
-      //     excluded_payment_types: [
-      //       {
-      //         id: "credit_card",
-      //       },
-      //       {
-      //         id: "debit_card",
-      //       },
-      //       { id: "atm" },
-      //       {
-      //         id: "ticket",
-      //       },
-      //     ],
-
-      //     installments: 1,
-      //     default_payment_method_id: "pix",
-      //   },
-      // });
-
-      const mpResponse = await mercadopago.payment.create({
-        transaction_amount: product.price,
-        payment_method_id: "pix",
-        installments: 1,
-
-        payer: {
-          email: product.userEmail,
+      const mpResponse = await mercadopago.preferences.create({
+        items: [
+          {
+            title: product.name,
+            unit_price: product.price,
+            quantity: product.quantity,
+            picture_url: product.image,
+          },
+        ],
+        binary_mode: true,
+        back_urls: {
+          success: `https://www.mxteras.com/payment/success/${orderId}`,
         },
-        callback_url: `https://mxteras.com/payment/success/${orderId}`,
+        auto_return: "all",
+        payment_methods: {
+          excluded_payment_types: [
+            {
+              id: "credit_card",
+            },
+            {
+              id: "debit_card",
+            },
+            { id: "atm" },
+            {
+              id: "ticket",
+            },
+          ],
+
+          installments: 1,
+          default_payment_method_id: "pix",
+        },
       });
+
+      // const mpResponse = await mercadopago.payment.create({
+      //   transaction_amount: product.price,
+      //   payment_method_id: "pix",
+      //   installments: 1,
+
+      //   payer: {
+      //     email: product.userEmail,
+      //   },
+      //   callback_url: `https://mxteras.com/payment/success/${orderId}`,
+      // });
 
       return mpResponse;
     } catch (error) {
